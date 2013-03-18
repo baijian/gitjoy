6ee1e57c4f34005c10740734fc4683c4c43162f5
柏健 <baijian@anjuke.com>
2013-03-18 11:05:04 +0800

    modify __init__ to add default config

--- src/demo/__init__.py
+++ src/demo/__init__.py
@@ -1,51 +1,89 @@
 # -*- coding: utf-8 -*-
+import os
 from flask import Flask, render_template, request
+
+def load_configuration(app):
+    config = app.config
+
+    from . import default_settings
+
+    config.from_object(default_settings)
+    if (os.environ.has_key("")):
+        config.from_envvar("", silent=True)
+    else:
+        path = os.path.abspath(os.path.expanduser(""))
+        if (os.path.isfile(path)):
+            config.from_pyfile(path, silent=True)
+        else:
+            config.from_pyfile("/etc/demo.conf", silent=True)
+
+def configure_loggers(app):
+    if app.debug:
+        import logging
+        
+        logger = logging.getLogger('sqlalchemy.engine')
+        logger.setLevel(logging.INFO)
+        logger.addHandler(logging.StreamHandler())
+
+def configure_views(app):
+    from .views import infos
+    app.register_blueprint(infos.mod)
 
 app = Flask(__name__)
 
-#
-# Configs
-#
-app.config.from_object("website_config")
-try:
-    app.config.from_pyfile(app.config['PRODUCTION_CONFIG'], silent=False)
-    print '[SUCCESS] load config file: ' + app.config['PRODUCTION_CONFIG']
-except:
-    pass
+load_configuration(app)
+configure_loggers(app)
 
-#
-# DB
-#
+#database
 from flask.ext.sqlalchemy import SQLAlchemy
+
 db = SQLAlchemy(app)
 
+__all__ = []
+
+##
+## Configs
+##
+#app.config.from_object("website_config")
+#try:
+#    app.config.from_pyfile(app.config['PRODUCTION_CONFIG'], silent=False)
+#    print '[SUCCESS] load config file: ' + app.config['PRODUCTION_CONFIG']
+#except:
+#    pass
 #
-# Login
+##
+## DB
+##
+#from flask.ext.sqlalchemy import SQLAlchemy
+#db = SQLAlchemy(app)
 #
-@app.route('/login', methods=['POST','GET'])
-def login():
-    error = None
-    if request.method == 'GET':
-        if request.args.get('username'):
-            error = 'same'
-        else:
-            error = 'different'
-    return render_template('login.html', error=error)
-
+##
+## Login
+##
+#@app.route('/login', methods=['POST','GET'])
+#def login():
+#    error = None
+#    if request.method == 'GET':
+#        if request.args.get('username'):
+#            error = 'same'
+#        else:
+#            error = 'different'
+#    return render_template('login.html', error=error)
 #
-# Blueprints
+##
+## Blueprints
+##
+#from demo.controllers import infos
+#app.register_blueprint(infos.mod)
 #
-from demo.controllers import infos
-app.register_blueprint(infos.mod)
-
-
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
-
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('404.html'),404
-
+#
+#@app.route('/hello')
+#@app.route('/hello/<name>')
+#def hello(name=None):
+#    return render_template('hello.html', name=name)
+#
+#@app.errorhandler(404)
+#def not_found(error):
+#    return render_template('404.html'),404
+#
 
