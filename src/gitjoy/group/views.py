b7c2c13dbb4dedc78d6a8957871c2bdbd23c2756
柏健 <jian.baij@gmail.com>
2013-07-25 23:58:19 +0800

    plan url of group

--- src/gitjoy/group/views.py
+++ src/gitjoy/group/views.py
@@ -6,13 +6,17 @@ from flask import Blueprint, render_template
 group = Blueprint('group', __name__)
 
 @group.route('/groups', methods = ["GET","POST"])
-def index():
+def groups():
     if request.method == 'POST':
         return render_template("group/index.html")   
     elif request.method =='GET':
         return render_template("group/index.html")
+
+@group.route('/groups/<id>', methods = ["GET"])
+def index():
     return render_template("group/index.html")
 
-@group.route('/groups/<id>/')
-def delete():
-    return render_template("")
+@group.route('/groups/new', methods = ["GET"])
+def new():
+    return render_template("group/index.html")
+
